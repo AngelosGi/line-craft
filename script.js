adde77ff6a4d66777c58be8f27c12c64b59a384c
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-11-25 23:03:01 +0200

    build grid

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const gridContainer = document.getElementById("grid-container")
 
-for (let i=0; i<32; i++){
-    const newGrid = document.createElement("div");
-    newGrid.classList.add("grid");
-    gridContainer.appendChild(newGrid);
+    for (let i=0; i<16; i++){
+    const row = document.createElement("div");
+    row.classList.add("grid");
+
+
+        for(let j=0; j<16; j++){
+        const coll = document.createElement("div");
+        coll.classList.add("grid");
+
+        row.appendChild(coll)
+
+    }   
+    
+        gridContainer.appendChild(row);
 }
+
+
 
 
