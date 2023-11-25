9d308bdac47fa659c558e91b40204127dae647aa
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-11-25 23:15:09 +0200

    fix function to get proper 16*16 grid

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const gridContainer = document.getElementById("grid-container")
 
+
+
+
     for (let i=0; i<16; i++){
     const row = document.createElement("div");
     row.classList.add("grid");
 
 
         for(let j=0; j<16; j++){
-        const coll = document.createElement("div");
-        coll.classList.add("grid");
+        const col = document.createElement("div");
+        col.classList.add("grid");
 
-        row.appendChild(coll)
+        row.appendChild(col);
 
     }   
-    
+
         gridContainer.appendChild(row);
 }
 
