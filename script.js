d8a09f65d46320c92fd5630c3fde5f174d910a4f
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-11-28 19:32:25 +0200

    add scalability to the grid boxes

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const gridContain = document.getElementById("gridContainer");
-let gridSize = 15;
+let gridSize = 20;
+
+
+// function changeGgridSize(newSize){
+//         gridSize = newSize
+//         createGrid();
+// }
+
+
+function createGrid(){
+
+    const boxSize = 70/gridSize;
 
     for (let rowIndex=0; rowIndex<gridSize; rowIndex++){
     const newRow = document.createElement("div");
@@ -8,13 +19,16 @@ let gridSize = 15;
         for(let columnIndex=0; columnIndex<gridSize; columnIndex++){
         const newCol = document.createElement("div");
         newCol.classList.add("grid-col-box");
+        newCol.style.height = `${boxSize}vw`;
+        newCol.style.width = `${boxSize}vw`;
+
         newRow.appendChild(newCol);
 
-    }   
+        }   
 
         gridContain.appendChild(newRow);
+    }
+
 }
-
-
-
-
+    
+    createGrid();
